ab546bd3ecf9de942eaa64d683e94ef0a94d6064
Brian W Bush <brian.bush@iohk.io>
2022-07-08 16:47:33 -0600

    Implemented `ToString` for `PlutusCore.Data`.

--- plutus/src/PlutusCore/Data.lean
+++ plutus/src/PlutusCore/Data.lean
@@ -13,15 +13,30 @@ inductive Data where
   | B      : ByteString → Data
 deriving Repr
 
-private def showData : Data → String
-  | Data.Constr n fs => "Constr " ++ toString n ++ " [" ++ String.intercalate "," (fs.map showData) ++ "]"
-  | Data.Map ps      => "Map  [" ++ String.intercalate "," (ps.map $ fun (k, v) => "(" ++ showData k ++ "," ++ showData v ++ ")") ++ "]" 
-  | Data.List xs     => "List [" ++ String.intercalate "," (xs.map showData) ++ "]"
-  | Data.I i         => "I " ++ toString i
-  | Data.B b         => "B " ++ toString b
+
+private def showWrapped (prefixTrue : String) (suffixTrue : String) (prefixFalse : String) (wrap : Bool) (x : String) : String :=
+  if wrap
+    then prefixTrue ++ x ++ suffixTrue
+    else prefixFalse ++ x
+
+private def mapWrapper'    := showWrapped "Map ["  "]" ","
+private def listWrapper'   := showWrapped "List [" "]" ","
+
+private def showData (wrap : Bool): Data → String
+  | Data.Constr n []         => "Constr " ++ toString n ++ " []"
+  | Data.Constr n (f :: [])  => "Constr " ++ toString n ++ " [" ++ showData true f ++ "]"
+  | Data.Constr n (f :: fs)  => "Constr " ++ toString n ++ " [" ++ showData true f ++ showData false (Data.List fs)
+  | Data.Map  []             => "Map []"
+  | Data.Map  ((p, q) :: []) => mapWrapper'  wrap $ "(" ++ showData true p ++ "," ++ showData true q ++ ")"
+  | Data.Map  ((p, q) :: ps) => mapWrapper'  wrap $ "(" ++ showData true p ++ "," ++ showData true q ++ ")" ++ showData false (Data.Map ps)
+  | Data.List []             => "List []"
+  | Data.List (x :: [])      => listWrapper' wrap $ showData true x
+  | Data.List (x :: xs)      => listWrapper' wrap $ showData true x ++ showData false (Data.List xs)
+  | Data.I    i              => "I " ++ toString i
+  | Data.B    b              => "B " ++ toString b
 
 instance : ToString Data where
-  toString := showData
+  toString := showData true
 
 
 end PlutusCore
